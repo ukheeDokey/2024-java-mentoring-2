6fdbde9e381875138ccb9647c33821fce39eb557
ukheeDokey <0514uh@gmail.com>
2025-01-14 18:50:02 +0900

    Refactor : firstNumber, secondNumber 입력 방식 수정

--- mission/mission1-calculator/src/main/java/com/calculator/controller/CalculatorController.java
+++ mission/mission1-calculator/src/main/java/com/calculator/controller/CalculatorController.java
@@ -7,40 +7,41 @@ import com.calculator.view.OutputView;
 public class CalculatorController {
     private final Calculator calculator;
     private final OutputView outputView;
-    private final int firstNumber;
-    private final int secondNumber;
+    private final InputView inputView;
 
     public CalculatorController(Calculator calculator, InputView inputView, OutputView outputView) {
         this.calculator = calculator;
         this.outputView = outputView;
-        this.firstNumber = inputView.firstNumber();
-        this.secondNumber = inputView.secondNumber();
+        this.inputView = inputView;
     }
 
-    private void addResult() {
+    private void addResult(int firstNumber, int secondNumber) {
         int add = calculator.add(firstNumber, secondNumber);
         outputView.printAddResult(add);
     }
 
-    private void subtractResult() {
+    private void subtractResult(int firstNumber, int secondNumber) {
         int subtract = calculator.subtract(firstNumber, secondNumber);
         outputView.printSubtractResult(subtract);
     }
 
-    private void multiplyResult() {
+    private void multiplyResult(int firstNumber, int secondNumber) {
         int multiply = calculator.multiply(firstNumber, secondNumber);
         outputView.printMultiplyResult(multiply);
     }
 
-    private void divideResult() {
+    private void divideResult(int firstNumber, int secondNumber) {
         int divide = calculator.divide(firstNumber, secondNumber);
         outputView.printDivideResult(divide);
     }
 
     public void run() {
-        addResult();
-        subtractResult();
-        multiplyResult();
-        divideResult();
+        int firstNumber = inputView.firstNumber();
+        int secondNumber = inputView.secondNumber();
+
+        addResult(firstNumber, secondNumber);
+        subtractResult(firstNumber, secondNumber);
+        multiplyResult(firstNumber, secondNumber);
+        divideResult(firstNumber, secondNumber);
     }
 }
